bf7026a54796af8bb3b274dbbbd62988feab8392
tedyhitti <thit436@cse.unsw.edu.au>
2014-10-23 00:48:01 +1100

    adding all data/tables/procedure/trigger relative to the question similarity check.. tested with new database without any issue.. just make sure u run the file in correct order..

---
+++ Confidence/SQL/similarity_check.sql
@@ -0,0 +1,100 @@
+DELIMITER //
+DROP function IF EXISTS  similarity_check //
+CREATE function similarity_check
+(q1 integer ,q2 integer) returns float
+BEGIN
+	DECLARE done integer DEFAULT 0;
+	declare q1_cur_word varchar(50);
+	declare q2_cur_word varchar(50);
+	declare totnbword integer default 0;
+	declare q1_len integer;
+	declare q2_len integer;
+	declare abs integer;
+	DECLARE matching integer default 0;
+	declare var integer default 0;
+	declare var1 integer default 0;
+	DECLARE exact_test  integer default 0;
+	DECLARE category_test  integer default 0;
+	DECLARE suffix_test  integer default 0;
+
+	DECLARE tot_matching integer default 0;
+	DECLARE word varchar(50); 
+	DECLARE suf varchar(20) ; 
+	DECLARE diff integer default 0;
+	DECLARE sameque integer default 0;
+
+	
+	DECLARE c_suffix CURSOR FOR select suffix from word_suffix ;
+	
+	DECLARE CONTINUE handler FOR NOT FOUND SET done =1;
+	
+	select count(*)
+	into exact_test
+	from question_d a, question_d b 
+	where a.id = q1 and b.id=q2 and a.word=b.word;
+
+
+	open c_suffix;
+	check_word : loop
+	fetch c_suffix into suf;
+	IF done = 1 then 
+		LEAVE check_word;
+	end if;	
+	
+	
+	select count(*) 
+	into matching
+	from question_d a, question_d b 
+	where a.id=q1 and b.id=q2 
+	and a.word not in
+			(select a.word from question_d a ,question_d b  where a.id=q1 and b.id=q2  and a.word=b.word)
+	and b.word not in
+			(select b.word from question_d a ,question_d b  where a.id=q1 and b.id=q2 and a.word=b.word)
+	and ( 
+		soundex(a.word)=soundex(b.word)
+	 or soundex(concat(a.word,suf))=soundex(b.word)
+	 or soundex(a.word)=soundex(concat(b.word,suf)) 
+		)
+	and b.category !=1
+	and a.category !=1;
+
+	-- set tot_matching =tot_matching + matching;
+	if matching > suffix_test then set suffix_test=matching; end if;
+	end loop;
+	close c_suffix;
+	
+	select count(*)
+	into category_test
+	from question_d a, question_d b 
+	where a.id = q1 and b.id=q2 
+	and a.word != b.word
+	and a.word not in ( select a.word from question_d a, question_d b where a.id = q1 and b.id=q2 and a.word=b.word)
+	and b.word not in ( select b.word from question_d a, question_d b where a.id = q1 and b.id=q2 and a.word=b.word)
+	and a.category = b.category and a.category >1;
+
+
+	
+	set tot_matching = category_test + exact_test + suffix_test;
+  -- return category_test; 	
+	select nbword into q1_len from question_h where id =q1;
+ 	select nbword into q2_len from question_h where id =q2;
+	select abs(q1_len - q2_len ) into abs ;-- from question_h where id =q2;
+	
+	if tot_matching >=q2_len or tot_matching >=q1_len then 
+		-- set tot_matching = -2; 
+		select count(distinct(word)) into var from question_d  where id in (q1,q2) and category =1;
+		select count(*) into var1 from question_d  where id in (q1,q2) and word='what';
+		if abs = 1   and var =1 then set tot_matching=3; end if;
+		if abs > 1 and var >1 then set tot_matching=-1; end if;
+		if abs=0 and (tot_matching = q2_len or tot_matching =q1_len) then set tot_matching = 4; end if;
+		if abs >2 then set tot_matching = 5;end if; 
+		-- if abs >= 1 and var =0 and var1=1 then set tot_matching=6; end if;
+		if abs >= 1 and var =0 then set tot_matching=6; end if;
+	else 
+		set tot_matching = -4; -- tot_matching *2/(q2_len+q1_len);
+	end if;
+
+	return    tot_matching;-- *2/(q2_len+q1_len); 
+END //
+DELIMITER ;
+
--- Confidence/SQL/table.sql
+++ Confidence/SQL/table.sql
@@ -1,7 +1,13 @@
-DROP TABLE `facilitators`;
-DROP TABLE `sittings`;
-DROP TABLE `questions`;
-DROP TABLE `votes_audit`;
+DROP TABLE if exists `facilitators`;
+DROP TABLE if exists `sittings`;
+DROP TABLE if exists `questions`;
+DROP TABLE if exists `votes_audit`;
+drop table if exists excluded_word;
+drop table if exists word_group;
+drop table if exists group_similarity;
+drop table if exists word_suffix;
+drop table if exists question_h;
+drop table if exists question_d;
 
 CREATE TABLE `facilitators` (
   `facilitator_id` INT NOT NULL AUTO_INCREMENT,
@@ -32,22 +38,55 @@ CREATE TABLE `questions` (
   `creation_time` DATETIME NULL,
   `post_time` DATETIME NULL,
   `post_date` DATETIME NULL,
+  `session_id` varchar(100),
   PRIMARY KEY (`que_id`)
   )
   ;
-alter table questions add session_id varchar(100);
+
 create table  `votes_audit` (
 	`sitting_id` INT NOT NULL,
 	`session_id` VARCHAR(45) NOT NULL,
 	`que_id` INT ,
 	`vote_date` DATETIME,
 	PRIMARY KEY (`que_id`,`session_id`,`sitting_id`)
-)
-	
-	 
+);
+
+-- Checking similarity 
+create table word_group ( 
+  category integer , 
+  word varchar(50)
+);
+
+create table group_similarity ( 
+  category integer , 
+  similarity integer
+);
+
+create table word_suffix ( 
+  id integer , 
+  suffix varchar(20)
+);
+
+create table question_h (
+  id integer , 
+  sentence varchar(100),
+  nbword integer,
+  sitting_id integer
+);
   
-  
-  
+create table question_d (
+  id integer,
+  word_id integer,
+  word_position integer,
+  word varchar(100),
+  category integer
+);
+
+create table excluded_word (
+  word varchar(100)
+);
+
+
   
   
   
